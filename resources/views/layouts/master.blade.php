7b72368685fe46a1ddcbe4dd6f6d891890dc51a0
Amir Mehrabi <aut0run2011@gmail.com>
2019-01-19 14:34:17 +0330

    Added a masterpage

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>دانه | با کتاب مورد علاقه‌ات آشنا شو</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/fonts/fonts.css">
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+
+    <section class="intro intro-overlay">
+      <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand " href="#">دانه</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto rtl pr-0">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">بخونش <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">ورود / ثبت نام</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">امکانات بخونش</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">انتخابات شما</a>
+            </li>
+
+          </ul>
+
+        </div>
+      </nav>
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col col-md-7 text-center">
+            <h1 class="h1 text-warning font-weight-bold mt-3 mb-3">دانه</h1>
+            <p class="text-light h5">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد
+            </p>
+            <p class="text-light h5 mt-4">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد.
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+
+    </section>
+
+    <section>
+      <p>تست</p>
+    </section>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+  </body>
+</html>
