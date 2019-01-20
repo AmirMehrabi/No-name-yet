c186f09da389fe1c6e7655bc902cc8285f614ce7
Amir Mehrabi <aut0run2011@gmail.com>
2019-01-20 13:01:52 +0330

    Made some minor changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,21 @@
-@extends('layouts.app')
+@extends('layouts.master')
+
+@include('layouts.navbar')
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div class="row content-justify-center">
+        <div class="col-md-8 ">
+            <div class="panel panel-default text-right">
+                <div class="panel-heading">داشبورد</div>
+                <div class="card" style="width: 18rem;">
+                  <div class="card-body">
+                    <h5 class="card-title">داشبورد</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                  </div>
                 </div>
             </div>
         </div>
